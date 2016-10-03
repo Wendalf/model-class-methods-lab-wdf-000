38accdc76d5db5522e0ac0db00655d4630ec96a8
Wendy Xu <guangw.xu@gmail.com>
2016-10-03 14:50:48 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,26 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+  
+  def self.catamaran_operators
+    includes(boats: :classifications).where('classifications.name = ?', 'Catamaran').uniq
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboat
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"}).uniq
+
+  end
+
+  def self.talented_seamen
+    self.sailors.where(id: self.motorboat.pluck(:id))
+  end
+
+  def self.non_sailors
+    where.not(id: self.sailors.pluck(:id))
+  end
+
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,13 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    where(id: self.pluck(:id))
+  end
+
+  def self.longest
+    Boat.order('length DESC').first.classifications
+  end
+
 end
